b20c522f986499daefa484007eabb1b782c3ee5c
Divishan <99882147+Divishan@users.noreply.github.com>
2022-02-17 18:32:54 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption =f' (file_name) \n @divishan23
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '➕our group➕', url="https://t.me/join சினிமா வெற்றித்துளி"
                                 )
                         ]
                     ]
